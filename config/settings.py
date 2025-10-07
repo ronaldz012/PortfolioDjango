4d4e790ceeefc2a0d61790b23e48a1ca128acace
ZIRA012 <91219113+ZIRA012@users.noreply.github.com>
2025-10-07 10:55:35 -0400

    update: gitignore

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,6 +131,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT ='/vol/web/static'
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
