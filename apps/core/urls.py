b1299f272b95dc02ef58d4ed9675d72771b3c414
ronaldz012 <rzepita02@gmail.com>
2025-10-13 09:56:44 -0400

    fix: projects route

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -5,7 +5,7 @@ app_name = 'core'
 urlpatterns = [
     path('',views.test, name='test'),
     path('home/',views.home, name='home'),
-    path("/projects", views.projects, name="projects"),
+    path("projects", views.projects, name="projects"),
     path('about/',views.about, name='about'),
     path('contact/',views.contact, name='contact'),    
 ]
